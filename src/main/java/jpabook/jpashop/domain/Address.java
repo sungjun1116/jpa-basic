db38f0a5b81db46b61a1abfc9938d69c9b269c8e
황성준 <devjun@wemakeprice.com>
2021-07-06 16:38:35 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 스펙상 만들어논 것을 티내기 위해 protected
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
