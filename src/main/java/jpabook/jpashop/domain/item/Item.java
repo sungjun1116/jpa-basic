38f79386a82c5045314f3eef8f8e9fa1a1500375
황성준 <devjun@wemakeprice.com>
2021-07-08 14:18:08 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,15 +37,22 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<Category>();
 
     //==비즈니스 로직==//
-//    public void addStock(int quantity) {
-//        this.stockQuantity += quantity;
-//    }
-//
-//    public void removeStock(int quantity) {
-//        int restStock = this.stockQuantity - quantity;
-//        if (restStock < 0) {
-//            throw new NotEnoughStockException("need more stock");
-//        }
-//        this.stockQuantity = restStock;
-//    }
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
